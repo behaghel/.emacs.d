2943c7bf315a0e96f975e591da6f8c05bd7e851a
Hubert Behaghel <behaghel@gmail.com>
2025-09-08 14:37:07 +0200

    [nav] Defer persp-switch to keep file visible; align Treemacs to project on create and on perspective switch; enable treemacs-follow-mode

--- modules/interactive/navigation/perspective-auto.el
+++ modules/interactive/navigation/perspective-auto.el
@@ -24,6 +24,11 @@
   :type 'boolean
   :group 'hub/persp-auto)
 
+(defcustom hub/persp-treemacs-follow t
+  "When non-nil, enable `treemacs-follow-mode' to track visited files."
+  :type 'boolean
+  :group 'hub/persp-auto)
+
 (defcustom hub/persp-ignore-roots nil
   "List of directory prefixes to ignore for auto perspectives."
   :type '(repeat directory)
@@ -66,10 +71,19 @@
 	    (unless (hub/persp--ignored-root-p root)
 	      proj)))))))
 
-(defun hub/persp--maybe-open-treemacs ()
-  "Open treemacs if available, ignore errors."
-  (when (and hub/persp-auto-open-treemacs (fboundp 'treemacs))
-    (ignore-errors (treemacs))))
+(defun hub/persp--treemacs-align-to-project (proj)
+  "Ensure Treemacs shows PROJ's root. Fall back to find-file alignment."
+  (when (fboundp 'treemacs)
+    (let ((default-directory (car (project-roots proj))))
+      (cond
+       ((fboundp 'treemacs-display-current-project-exclusively)
+	(ignore-errors (treemacs-display-current-project-exclusively)))
+       ((fboundp 'treemacs-add-and-display-current-project)
+	(ignore-errors (treemacs-add-and-display-current-project)))
+       ((fboundp 'treemacs-find-file)
+	(ignore-errors (treemacs-find-file))))
+      (when (and hub/persp-treemacs-follow (fboundp 'treemacs-follow-mode))
+	(treemacs-follow-mode 1)))))
 
 (defun hub/persp--visit-file-handler ()
   "Hook to create/switch to project perspective when visiting files."
@@ -99,11 +113,25 @@
   :global t
   :group 'hub/persp-auto
   (if hub/persp-auto-project-mode
-      (add-hook 'find-file-hook #'hub/persp--visit-file-handler)
-    (remove-hook 'find-file-hook #'hub/persp--visit-file-handler)))
+      (progn
+	(add-hook 'find-file-hook #'hub/persp--visit-file-handler)
+	(add-hook 'persp-switch-hook #'hub/persp--align-treemacs-on-persp-switch))
+    (remove-hook 'find-file-hook #'hub/persp--visit-file-handler)
+    (remove-hook 'persp-switch-hook #'hub/persp--align-treemacs-on-persp-switch)))
 
 ;; Enable by default in interactive sessions
 (hub/persp-auto-project-mode 1)
 
 (provide 'navigation/perspective-auto)
 ;;; perspective-auto.el ends here
+
+
+(defun hub/persp--align-treemacs-on-persp-switch (_new _frame)
+  "Align Treemacs to a project's root when the perspective changes."
+  (when (and (fboundp 'treemacs) (buffer-live-p (current-buffer)))
+    (let* ((buf (current-buffer))
+	   (fname (buffer-file-name buf)))
+      (when fname
+	(let ((proj (project-current nil)))
+	  (when proj
+	    (hub/persp--treemacs-align-to-project proj))))))
