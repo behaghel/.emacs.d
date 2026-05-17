4511239082796c074b4812f6293104a7d0f9c7ed
Hubert Behaghel <behaghel@gmail.com>
2026-05-17 14:50:02 +0200

    [notes] defer Denote package loading
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- modules/interactive/notes/brain.el
+++ modules/interactive/notes/brain.el
@@ -7,31 +7,43 @@
 
 (require 'hub-utils)
 
+(defgroup hub/notes nil
+  "Notes configuration for Denote."
+  :group 'denote)
+
+(defcustom hub/denote-directory (expand-file-name "~/ws/blog.behaghel.org/content-org/journal/")
+  "Default directory for Denote notes."
+  :type 'directory
+  :group 'hub/notes)
+
+(defun hub/denote--configure-org-capture ()
+  "Configure Org capture templates that create Denote notes."
+  (setq denote-org-capture-specifiers "%l\n%i\n%?")
+  (add-to-list 'org-capture-templates
+	       '("n" "New note (with denote.el)" plain
+		 (file denote-last-path)
+		 #'denote-org-capture
+		 :no-save t :immediate-finish nil :kill-buffer t :jump-to-captured t)))
+
 (use-package denote
-  :config
-  (evil-global-set-key 'normal ",no" #'denote-open-or-create)
-  (evil-global-set-key 'normal ",nn" #'denote)
-  (evil-global-set-key 'normal ",nt" #'denote-type)
-  (evil-global-set-key 'normal ",nd" #'denote-date)
-  (evil-global-set-key 'normal ",ns" #'denote-subdirectory)
-  (evil-global-set-key 'normal ",nt" #'denote-template)
-  (evil-global-set-key 'normal ",nr" #'denote-rename-file)
-
-  (evil-collection-define-key 'normal 'org-mode-map
-			      ",nl" #'denote-link-or-create
-			      ",nL" #'denote-link-add-links
-			      ",nb" #'denote-backlinks
-			      ",nf" #'denote-link-find-file
-			      ",nB" #'denote-link-find-backlink
-			      ",nR" #'denote-rename-file-using-front-matter)
-
-  (defgroup hub/notes nil
-    "Notes configuration for Denote."
-    :group 'denote)
-  (defcustom hub/denote-directory (expand-file-name "~/ws/blog.behaghel.org/content-org/journal/")
-    "Default directory for Denote notes."
-    :type 'directory
-    :group 'hub/notes)
+  :defer t
+  :commands (denote
+	     denote-backlinks
+	     denote-date
+	     denote-dired-mode-in-directories
+	     denote-fontify-links-mode-maybe
+	     denote-link-find-backlink
+	     denote-link-find-file
+	     denote-link-add-links
+	     denote-link-or-create
+	     denote-open-or-create
+	     denote-org-capture
+	     denote-rename-file
+	     denote-rename-file-using-front-matter
+	     denote-subdirectory
+	     denote-template
+	     denote-type)
+  :init
   (setq denote-directory hub/denote-directory
 	denote-known-keywords '("emacs" "faith" "family" "hubert" "pro" "engineering" "leadership")
 	denote-infer-keywords t
@@ -42,19 +54,30 @@
 	denote-date-prompt-use-org-read-date t
 	denote-allow-multi-word-keywords nil
 	denote-date-format nil
-	denote-backlinks-show-context t)
+	denote-backlinks-show-context t
+	denote-dired-directories (list denote-directory))
+
+  (evil-global-set-key 'normal ",no" #'denote-open-or-create)
+  (evil-global-set-key 'normal ",nn" #'denote)
+  (evil-global-set-key 'normal ",nt" #'denote-type)
+  (evil-global-set-key 'normal ",nd" #'denote-date)
+  (evil-global-set-key 'normal ",ns" #'denote-subdirectory)
+  (evil-global-set-key 'normal ",nt" #'denote-template)
+  (evil-global-set-key 'normal ",nr" #'denote-rename-file)
+
+  (with-eval-after-load 'evil-collection
+    (evil-collection-define-key 'normal 'org-mode-map
+				",nl" #'denote-link-or-create
+				",nL" #'denote-link-add-links
+				",nb" #'denote-backlinks
+				",nf" #'denote-link-find-file
+				",nB" #'denote-link-find-backlink
+				",nR" #'denote-rename-file-using-front-matter))
 
   (add-hook 'text-mode-hook #'denote-fontify-links-mode-maybe)
-  (setq denote-dired-directories (list denote-directory))
   (add-hook 'dired-mode-hook #'denote-dired-mode-in-directories)
-
   (with-eval-after-load 'org-capture
-    (setq denote-org-capture-specifiers "%l\n%i\n%?")
-    (add-to-list 'org-capture-templates
-		 '("n" "New note (with denote.el)" plain
-		   (file denote-last-path)
-		   #'denote-org-capture
-		   :no-save t :immediate-finish nil :kill-buffer t :jump-to-captured t))))
+    (hub/denote--configure-org-capture)))
 
 (provide 'notes/brain)
 ;;; brain.el ends here
